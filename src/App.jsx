065106f817f4d3cf9048e1c4162affb5d588158f
giash459 <mdgiash999ctg@gmail.com>
2025-09-20 11:09:09 +0600

    finished the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 
+import { Suspense } from 'react'
 import './App.css'
 import Countries from './components/Countries/Countries'
+
+const countriesPromise = fetch('https://openapi.programming-hero.com/api/all')
+  .then(res => res.json())
 
 function App() {
 
   return (
     <>
-      <Countries></Countries>
+      {/* <p>https://github.com/giash459/react-world-on-the-go</p> */}
+      <Suspense fallback={<p>Nadir vai loading.....</p>}>
+        <Countries countriesPromise={countriesPromise}></Countries>
+      </Suspense>
     </>
   )
 }
---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import './Country.css'
+
+const Country = ({ country, handleVisitedCountries, handleVisitedFlags }) => {
+    const [visited, setVisited] = useState(false);
+    // console.log(country)
+
+    const handleVisited = () => {
+        // console.log('button Clicked');
+        // basic system
+        // if(visited){
+        //     setVisited(false)
+        // }
+        // else{
+        //     setVisited(true)
+        // }
+
+        //second system
+        // setVisited(visited ? false : true);
+
+        // third system
+        setVisited(!visited);
+        handleVisitedCountries(country);
+    }
+
+    return (
+        // <div className={`country border-lg text-center ${visited ? 'country-visited' : 'not-country-visited'}`}>
+        <div className={`country ${visited && 'country-visited'}`}>
+            <img src={country?.flags?.flags?.png} alt={country.flags.flags.alt} />
+            <h3>Name : {country.name.common}</h3>
+            <p>Population : {country.population.population}</p>
+            <p>Area : {country.area.area} {country.area.area > 300000 ? 'Big Country' : 'Small Country'}</p>
+            <button onClick={handleVisited}>
+                {visited ? 'Visited' : 'Not Visited'}
+            </button>
+            <button onClick={() => { handleVisitedFlags(country?.flags?.flags?.png) }}>Add Visited Flag</button>
+        </div>
+    );
+};
+
+export default Country;
